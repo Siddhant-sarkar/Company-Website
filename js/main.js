a843b781ab00bd69db4dc137f09b8e318841f08d
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-01-04 13:04:23 +0530

    Web Site Made

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let map;
+function initMap() {
+    const loc={lat:42.364168 ,lng: -71.46828};
+    const map = new google.maps.Map(document.querySelector('.map')
+    , {
+    center: loc,
+    zoom: 14,
+  });
+    const marker=new google.maps.Marker({position:loc ,map:map});
+}
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
